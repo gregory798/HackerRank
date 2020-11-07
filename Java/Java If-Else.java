956a636c7cc89b231e3e927d356b0891354c64d0
gregory798 <73400124+gregory798@users.noreply.github.com>
2020-11-08 00:58:27 +0100

    Create Java If-Else.java

---
+++ Java/Java If-Else.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (x >= 1 && x <= 100){
+            if (x%2 != 0){
+                System.out.println("Weird");
+            }else if (x >= 2 && x <= 5){
+                System.out.println("Not Weird");
+            }else if (x >= 6 && x <= 20){
+                System.out.println("Weird");
+            }else if (x>20){
+                System.out.println("Not Weird");
+            }
+
+        }else{
+            System.out.print("Fuck...");
+            }
+
+
+
+
+
+
+
+        scanner.close();
+    }
+}
